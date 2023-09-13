b6d8d7bba979c1b8383c97c94bd4e54e63cfcd3f
Ougrid D. <ougridd@gmail.com>
2023-09-13 15:06:45 +0700

    add `GetPolicyListByPremIn`

--- report/report/Services/PolicySevice.cs
+++ report/report/Services/PolicySevice.cs
@@ -17,8 +17,32 @@ namespace report.Services
             string agentCode = data.agentCode;
             DateTime startDate = data.startDate;
             DateTime endDate = data.endDate;
+
             var policyList = await _dbService.GetAll<Policy>("SELECT * FROM static_data.\"Policies\" Where \"agentCode\" = @agentCode And \"createdAt\" >= @startDate And \"createdAt\" <= @endDate",
-                new {agentCode = agentCode,
+                new
+                {
+                    agentCode = agentCode,
+                    startDate = startDate,
+                    endDate = endDate
+                });
+            return policyList;
+        }
+
+        public async Task<List<Policy>> GetPolicyListByPremIn(Billing data)
+        {
+            string agentCode = data.agentCode;
+            DateTime startDate = data.startDate;
+            DateTime endDate = data.endDate;
+            const cond = '';
+            if (data.startDate)
+            {
+                cond = cond + ' and startdate = ' + data.startdate
+            }
+
+            var policyList = await _dbService.GetAll<Policy>("SELECT * FROM static_data.\"Policies\" Where \"agentCode\" = @agentCode And \"createdAt\" >= @startDate And \"createdAt\" <= @endDate" + cond,
+                new
+                {
+                    agentCode = agentCode,
                     startDate = startDate,
                     endDate = endDate
                 });
