e81566a1db9b472271589b2b621f0645d4b4c09a
ougrid <39577404+ougrid@users.noreply.github.com>
2023-09-13 13:37:21 +0700

    edit `PremInReport` model

--- report/report/Models/PremInReport.cs
+++ report/report/Models/PremInReport.cs
@@ -5,7 +5,25 @@ namespace report.Models
 {
     public class PremInReport
     {
-        [Key] public int Id { get; set; }
-        public string transType { get; set; } = null!;
+        [Key] public int Id { get; set; };
+        public string policyNo { get; set; } = null!;
+        public string invoiceNo { get; set; };
+        public string seqNo { get; set; };
+        public string cashierNo { get; set; };
+        public string cashierAmt { get; set; };
+        public string cashierReceiveType { get; set; };
+        public string cashierRefNo { get; set; };
+        public DateTime cashierRefDate { get; set; };
+        public string premInDebtWriteOffNo { get; set; };
+        public DateTime premInDebtWriteOffDate { get; set; };
+        public string netFlag { get; set; };
+        public float premInDebtWriteOffAmt { get; set; };
+        public float diffAmt { get; set; };
+        puclic string status { get; set; };
+        public DateTime effectiveDate { get; set; };
+        public DateTime expiryDate { get; set; };
+        public string advisorCode { get; set; };
+        public string advisorName { get; set; };
+        public string customerId { get; set; };
     }
 }
