7e730e6755b3c516461b69c23958f374aef2e1c7
Ougrid D. <ougridd@gmail.com>
2023-09-15 08:51:23 +0700

    change nullable fields in `Transaction`

--- report/report/Models/PremInReport.cs
+++ report/report/Models/PremInReport.cs
@@ -5,25 +5,29 @@
 // {
 //     public class PremInReport
 //     {
-//         [Key] public int Id { get; set; };
+//         [Key] public int Id { get; set; }
 //         public string policyNo { get; set; } = null!;
-//         public string invoiceNo { get; set; };
-//         public string seqNo { get; set; };
-//         public string cashierNo { get; set; };
-//         public string cashierAmt { get; set; };
-//         public string cashierReceiveType { get; set; };
-//         public string cashierRefNo { get; set; };
-//         public DateTime cashierRefDate { get; set; };
-//         public string premInDebtWriteOffNo { get; set; };
-//         public DateTime premInDebtWriteOffDate { get; set; };
-//         public string netFlag { get; set; };
-//         public float premInDebtWriteOffAmt { get; set; };
-//         public float diffAmt { get; set; };
-//         public string status { get; set; };
-//         public DateTime effectiveDate { get; set; };
-//         public DateTime expiryDate { get; set; };
-//         public string advisorCode { get; set; };
-//         public string advisorName { get; set; };
-//         public string customerId { get; set; };
+//         public string invoiceNo { get; set; } = null!;
+//         public string seqNo { get; set; } = null!;
+//         public string cashierNo { get; set; } = null!;
+//         public string cashierAmt { get; set; } = null!;
+//         public string cashierReceiveType { get; set; } = null!;
+//         public string cashierRefNo { get; set; } = null!;
+//         public DateTime cashierRefDate { get; set; }
+//         public string premInDebtWriteOffNo { get; set; } = null!;
+//         public DateTime premInDebtWriteOffDate { get; set; }
+//         public string netFlag { get; set; } = null!;
+//         public float premInDebtWriteOffAmt { get; set; }
+//         public float diffAmt { get; set; }
+//         public string status { get; set; }
+//         public DateTime effectiveDate { get; set; }
+//         public DateTime expiryDate { get; set; }
+//         public string advisorCode { get; set; } = null!;
+//         public string advisorName { get; set; } = null!;
+//         public string customerId { get; set; } = null!;
+
+
+//         public string insurerCode { get; set; }
+//         public string agentCode { get; set; }
 //     }
 // }
--- report/report/Models/Transaction.cs
+++ report/report/Models/Transaction.cs
@@ -3,45 +3,53 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace amityReport.Models
 {
-    public class Transaction
-    {
-        [Key] public int Id { get; set; }
-        public string transType { get; set; } = null!;
-        public string transStatus { get; set; } = null!;
-        public int subType { get; set; }
-        public string insurerCode { get; set; } = null!;
-        public string agentGroupCode { get; set; } = null!;
-        public string policyNo { get; set; } = null!;
-        public string agentCode { get; set; } = null!;
-        public float amount { get; set; }
-        public float duty { get; set; }
-        public float stamp { get; set; }
-        public float total { get; set; }
-        public DateTime payDate { get; set; }
-        public string payNo { get; set; } = null!;
-        public DateTime invoiceDate { get; set; }
-        public string invoiceNo { get; set; } = null!;
-        public string endoseNo { get; set; } = null!;
-        public int level { get; set; }
-        public DateTime dueDate { get; set; }
-        public DateTime createdAt { get; set; }
-        public DateTime updatedAt { get; set; }
-        public int groupSatmentID { get; set; }
-        public float totalamt { get; set; }
+  public class Transaction
+  {
+    [Key] public int Id { get; set; }
+    public string? transType { get; set; } = null!;
+    // public string transType { get; set; }
+    // public string transStatus { get; set; } = null!;
+    public string? transStatus { get; set; }
+    public int subType { get; set; }
+    public string? insurerCode { get; set; } = null!;
+    // public string agentGroupCode { get; set; } = null!;
+    public string? agentGroupCode { get; set; }
+    // public string policyNo { get; set; } = null!;
+    public string? policyNo { get; set; }
+    public string? agentCode { get; set; } = null!;
+    public float amount { get; set; }
+    public float duty { get; set; }
+    public float stamp { get; set; }
+    public float total { get; set; }
+    public DateTime payDate { get; set; }
+    // public string payNo { get; set; } = null!;
+    public string? payNo { get; set; }
+    public DateTime invoiceDate { get; set; }
+    // public string invoiceNo { get; set; } = null!;
+    public string? invoiceNo { get; set; }
+    // public string endoseNo { get; set; } = null!;
+    public string? endoseNo { get; set; }
+    public int level { get; set; }
+    public DateTime dueDate { get; set; }
+    public DateTime createdAt { get; set; }
+    public DateTime updatedAt { get; set; }
+    public int groupSatmentID { get; set; }
+    public float totalamt { get; set; }
 
-         public float paidamt { get; set; }
-         public float remainamt { get; set; }
-        public string status { get; set; } = null!;
-        public int seqno { get; set; }
-        public float commamt { get; set; }
-         public float commtaxamt { get; set; }
-       public float ovamt { get; set; }
-         public float ovtaxamt { get; set; }
-        public DateTime rprefdate { get; set; }
+    public float paidamt { get; set; }
+    public float remainamt { get; set; }
+    // public string status { get; set; } = null!;
+    public string? status { get; set; }
+    public int seqno { get; set; }
+    public float commamt { get; set; }
+    public float commtaxamt { get; set; }
+    public float ovamt { get; set; }
+    public float ovtaxamt { get; set; }
+    public DateTime rprefdate { get; set; }
 
-        [Column("premin-rprefdate")] public DateTime Preminrprefdate { get; set; }
+    [Column("premin-rprefdate")] public DateTime Preminrprefdate { get; set; }
 
-        [Column("premout-rprefdate")] public DateTime Premoutrprefdate { get; set; }
+    [Column("premout-rprefdate")] public DateTime Premoutrprefdate { get; set; }
 
-    }
+  }
 }
